14115b6c267ace85dc96e242790ff88e92b6ee7f
Ian Irwin <Ianiriwn18@gmail.com>
2022-09-01 13:01:33 -0700

    adding backend server and login/register function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Crew } from "./components/Crew";
 import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
+import Register from "./components/Register";
 import { Footer } from "./components/Footer";
 import { Tutorial } from "./components/Tutorial";
 import { Game } from "./components/Game";
-import { Login } from "./components/Login";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-
+import Login from "./components/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -20,16 +18,20 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route path="/" element={<>
-              <Banner />
-              <Crew />
-              <Projects />
-              <Contact />
-              <Login />
-            </>} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Banner />
+                <Crew />
+                <Projects />
+                <Register />
+                <Login />
+              </>
+            }
+          />
           <Route path="/Tutorial" element={<Tutorial />} />
-         
-        </Routes>      
+        </Routes>
         <Footer />
       </BrowserRouter>
     </div>
@@ -37,6 +39,3 @@ function App() {
 }
 
 export default App;
-
-
-
