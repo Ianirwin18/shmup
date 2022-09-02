2de4e76284fd48bd6c6825691704497888a8ef00
Ian Irwin <Ianiriwn18@gmail.com>
2022-09-01 18:52:34 -0700

    fixed merge changes

--- src/components/Crew.js
+++ src/components/Crew.js
@@ -1,13 +1,21 @@
 import meter1 from "../assets/img/buzz.png";
 import meter2 from "../assets/img/courage.png";
 import meter3 from "../assets/img/goku.png";
+import meter4 from "../assets/img/goku.png";
+import meter5 from "../assets/img/goku.png";
+import meter6 from "../assets/img/goku.png";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
+import { Link, useNavigate } from "react-router-dom";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+
 
 export const Crew = () => {
+  
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
@@ -27,35 +35,51 @@ export const Crew = () => {
       items: 1
     }
   };
-
+  let navigate = useNavigate();
   return (
     <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Meet the Crew</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Warrant Officer</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Space Commander</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Lt. Colonel</h5>
-                            </div>
-  
-                        </Carousel>
-                    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Meet the Crew</h2>
+              <p>The three Leads of the Programming Elite Squardian team you'll find are bellow.<br></br> The three experimental on board S.H.I.P. AIs you will also find down bellow. Click on anyones portrait to learn more all about them. --J.L.2473</p>
+              <button type="button" onClick={() => navigate("/Bio")}>
+                Learn More
+                <ArrowRightCircle size={25} />
+              </button>
+              <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                <div className="item">
+                  <img src={meter1} alt="Image" />
+                  <h5>Warrant Officer</h5>
                 </div>
+                <div className="item">
+                  <img src={meter2} alt="Image" />
+                  <h5>Space Commander</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="Image" />
+                  <h5>Lt. Colonel</h5>
+                </div>
+                <div className="item">
+                  <img src={meter4} alt="Image" />
+                  <h5>Web Sibilings</h5>
+                </div>
+                <div className="item">
+                  <img src={meter5} alt="Image" />
+                  <h5>The Snakes</h5>
+                </div>
+                <div className="item">
+                  <img src={meter6} alt="Image" />
+                  <h5>The Nonexistent</h5>
+                </div>
+
+              </Carousel>
             </div>
+          </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
   )
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -6,7 +6,7 @@ export const Game = () => {
        
         <div>
         <iframe src='https://itch.io/embed-upload/6359148?color=742e82' allowfullscreen='' width='640' height='380'frameborder='0' title="Level 1"><a href='https://ianirwin18.itch.io/testgame'>Play testgame on itch.io</a></iframe>
-          </div>
+        </div>
         
         
 
