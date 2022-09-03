cd86b793b24a2b295a65160e2a827cfb0be2e562
Connor Mitchener <lightsoutconnor1@gmail.com>
2022-09-02 20:30:06 -0700

    finished bio sort of

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -1,6 +1,7 @@
 import "../Tutorial.css";
-import buzz from "../assets/img/buzz.png";
-import goku from "../assets/img/goku.png";
+import chad from "../assets/img/chadchat.png";
+import erik from "../assets/img/erikchat.png";
+import nick from "../assets/img/nickchat.png";
 
 
 export const Tutorial = () => {
@@ -14,7 +15,7 @@ export const Tutorial = () => {
                         <div class="mesgs">
                             <div class="msg_history">
                                 <div class="incoming_msg">
-                                    <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"></img> </div>
+                                    <div class="incoming_msg_img"> <img src={ chad } alt="sunil"></img> </div>
                                     <div class="received_msg">
                                         <div class="received_withd_msg">
                                             <p>Welcome new recruits to the S.H.I.P. program I’m Space Commander C.T.9532 but you can call me Chad. Do you have any experience piloting S.H.I.P.s?</p>
@@ -29,7 +30,7 @@ export const Tutorial = () => {
                                         <span class="time_date"> 11:01 AM    |    June 9</span> </div>
                                 </div>
                                 <div class="incoming_msg">
-                                    <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"></img> </div>
+                                    <div class="incoming_msg_img"> <img src={ chad } alt="sunil"></img> </div>
                                     <div class="received_msg">
                                         <div class="received_withd_msg">
                                             <p> Well a S.H.I.P. is a S.H.I.P. but nevermind that lets start with the basics you move the S.H.I.P. by using your W,A,S,D keys W to move upwards S to move down D to move forward and A to move back.
@@ -43,7 +44,7 @@ export const Tutorial = () => {
                                         <span class="time_date"> 11:01 AM    |    Today</span> </div>
                                 </div>
                                 <div class="incoming_msg">
-                                    <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"></img> </div>
+                                    <div class="incoming_msg_img"> <img src={ nick } alt="sunil"></img> </div>
                                     <div class="received_msg">
                                         <div class="received_withd_msg">
                                             <p> This is Warrant Officer N.G.8434 reporting in Chad did you not teach the new recruit about the weapons system yet they need to be ready to deploy in Three minutes. Listen new recruit, you can fire the S.H.I.P.s on board weapon system by pressing or holding the SPACEBAR. By the way, you can call me Nick.</p>
@@ -56,7 +57,7 @@ export const Tutorial = () => {
                                         <span class="time_date"> 11:01 AM    |    Today</span> </div>
                                 </div>
                                 <div class="incoming_msg">
-                                    <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"></img> </div>
+                                    <div class="incoming_msg_img"> <img src={ erik } alt="erik"></img> </div>
                                     <div class="received_msg">
                                         <div class="received_withd_msg">
                                             <p>Hold on, recruit, don't run off yet Lt. Colonel E.H.7634 reporting in. While Nick here taught you how to shoot he didn’t get the chance to teach you about the weapons overdrive systems. If you hold down the F key you can overload the weapon system to shoot out a large beam. But you can also hit the E key to switch the Beam to a Hyper Overdrive when you finish charging the beam it’ll fire larger normal shots but you will overheat the system's energy and won't be able to charge for a short period of time afterwards. 
@@ -70,10 +71,10 @@ export const Tutorial = () => {
                                         <span class="time_date"> 11:01 AM    |    Today</span> </div>
                                 </div>
                                 <div class="incoming_msg">
-                                    <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"></img> </div>
+                                    <div class="incoming_msg_img"> <img src={ nick } alt="sunil"></img> </div>
                                     <div class="received_msg">
                                         <div class="received_withd_msg">
-                                            <p> Ohh shoot one last thing, recruit you’ll find upgrades when you’re out on missions the portable gun upgrade will attach to your S.H.I.P. on either the front or back of the S.H.I.P. if you press the Q key you can detach the portable gun and can reattach it to either side. Worry not recruit when you detach the portable gun it will slowly work its way back to your ship or you can press Q again to make it return to you faster.
+                                            <p> Ohh shoot one last thing, Recruit, you’ll find upgrades when you’re out on missions the portable gun upgrade will attach to your S.H.I.P. on either the front or back of the S.H.I.P. if you press the Q key you can detach the portable gun and can reattach it to either side. Worry not recruit when you detach the portable gun it will slowly work its way back to your ship or you can press Q again to make it return to you faster.
 
  </p>
                                             <span class="time_date"> 11:01 AM    |    Today</span></div>
@@ -85,7 +86,7 @@ export const Tutorial = () => {
                                         <span class="time_date"> 11:01 AM    |    Today</span> </div>
                                 </div>
                                 <div class="incoming_msg">
-                                    <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"></img> </div>
+                                    <div class="incoming_msg_img"> <img src={ chad } alt="sunil"></img> </div>
                                     <div class="received_msg">
                                         <div class="received_withd_msg">
                                             <p>  Whew…… I hope we covered everything. I know Inspector B.A.0762 is going to be checking these logs closely, I’m sure he’d tell us if we forgot anything important.
